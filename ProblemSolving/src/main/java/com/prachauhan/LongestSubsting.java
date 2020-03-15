eb9a759732dca38b4e541dcc27c32c03aaa13871
Prachi Chauhan <prachi-chauhan@users.noreply.github.com>
2020-03-15 10:58:07 -0500

    Boba (#1)
    * Covering Majority Test Cases
    
    * Covering All Test Cases 13 ms 41.9 MB

--- ProblemSolving/src/main/java/com/prachauhan/LongestSubsting.java
+++ ProblemSolving/src/main/java/com/prachauhan/LongestSubsting.java
@@ -1,7 +1,14 @@
 package com.prachauhan;
 
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 
+/**
+ * Cases covered: "aaaa","abcabc",""," "
+ *
+ * @author prachichauhan
+ */
 public class LongestSubsting {
     private static int length;
     
@@ -19,18 +26,34 @@ public class LongestSubsting {
     
     public static int lengthOfLongestSubstring(String s) {
         ArrayList<Character> result = new ArrayList();
-        ArrayList<Character> temp = new ArrayList();
-        if (s.trim().length() == 0 || s == null) {
+        ArrayList<Character> temp = new ArrayList<>(s.length());
+        for (int i = 0; i < s.length(); i++) {
+            temp.add(s.charAt(i));
+        }
+        if (s.length() == 0 || s == null) {
             return 0;
+        } else if (s.length() == 1) {
+            return 1;
         } else {
+            int index;
             for (int i = 0; i < s.length(); i++) {
-                if (temp.contains(s.charAt(i))) {
-                    result = temp;
-                    if (temp.size() > result.size()) {
-                        temp = null;
-                        continue;
+                if (!result.contains(temp.get(i))) {
+                    result.add(temp.get(i));
+                } else {
+                    index=result.indexOf(temp.get(i));
+                    if (result.get(0) == temp.get(i)) {
+                        result.remove(0);
+                        result.add(temp.get(i));
                     } else {
-                        temp.add(s.charAt(i));
+//                        case where the duplicate character is in the middle of the string consecutively :"abccdefg" "pwwkew"
+//                        substring from index of last occurrence to include cases like "ohvhjdml" and "wobgrovw"
+                        String substring = s.substring(index);
+                        //                        case where the duplicate character is in the middle of the string non-consecutively :"ckilbkd"
+                        int res = lengthOfLongestSubstring(substring);
+                        if (res > result.size()) {
+                            return res;
+                        }
+                        break;
                     }
                 }
             }
--- ProblemSolving/src/test/java/com/prachauhan/LongestSubstringTest.java
+++ ProblemSolving/src/test/java/com/prachauhan/LongestSubstringTest.java
@@ -7,7 +7,15 @@ public class LongestSubstringTest {
     @Test
     public void longestSubstring() {
         String s="abcabcbb";
-        LongestSubsting longestSubsting = new LongestSubsting(s);
-        System.out.println(longestSubsting.getLength());
+        String s1 ="aaaaa";
+        String s2="pwwkew";
+        String s3="dvdf";
+        String s4=" ";
+        String [] arr={s,s1,s2,s3, s4, "ckilbkd","ohvhjdml"};
+        for (String i : arr){
+            LongestSubsting longestSubsting = new LongestSubsting(i);
+            System.out.println(longestSubsting.getLength());
+        }
+      
     }
 }
