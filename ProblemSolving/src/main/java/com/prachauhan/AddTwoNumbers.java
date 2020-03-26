143ce33f331d7adcee652a6c9a2dc29fee67c40e
prachi-chauhan <prachichauhan777@gmail.com>
2020-03-25 22:54:46 -0500

    Add two numbers given as LinkedList

---
+++ ProblemSolving/src/main/java/com/prachauhan/AddTwoNumbers.java
@@ -0,0 +1,36 @@
+package com.prachauhan;
+
+public class AddTwoNumbers {
+    
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode head = new ListNode(0);
+        ListNode p = l1, q = l2, curr = head;
+        int nodeCarry = 0;
+        while (p != null || q != null) {
+            int x = (p != null) ? p.val : 0;
+            int y = (q != null) ? q.val : 0;
+            
+            int nodeSum = (x + y + nodeCarry) % 10;
+            nodeCarry = (x + y + nodeCarry) / 10;
+            
+            curr.next = new ListNode(nodeSum);
+            curr = curr.next;
+            
+            if (p != null) p = p.next;
+            if (q != null) q = q.next;
+        }
+        if (nodeCarry > 0) {
+            curr.next = new ListNode(nodeCarry);
+        }
+        return head.next;
+    }
+}
+
+class ListNode {
+    int val;
+    ListNode next;
+    
+    ListNode(int x) {
+        val = x;
+    }
+}
---
+++ ProblemSolving/src/test/java/com/prachauhan/AddTwoNumbersTest.java
@@ -0,0 +1,21 @@
+package com.prachauhan;
+
+import org.junit.Test;
+
+public class AddTwoNumbersTest {
+    
+    @Test
+    public void addingTwoNumbersTest(){
+        AddTwoNumbers addingTwoNumbers=new AddTwoNumbers();
+//        ListNode listNode=addingTwoNumbers.addTwoNumbers();
+    }
+}
+
+class ListNode {
+    int val;
+    ListNode next;
+    
+    ListNode(int x) {
+        val = x;
+    }
+}
